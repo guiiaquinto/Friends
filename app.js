9471f752d631564d101e36c46ca4cd423786ae48
Guilherme Anzolin Iaquinto <85501131+guiiaquinto@users.noreply.github.com>
2021-12-06 23:42:34 -0300

    Correção

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function gerar() {
     var nEpisodio = episodios[Math.floor(Math.random() * episodios.length)];
     (episodios.length)
     var eps = document.querySelector('h2#res');
-    eps.innerHTML = `<p>O episódio que você tem que assistir é da <strong>${nEpisodio.temporada}º Temporada, episódio ${nEpisodio.episodio}, ${nEpisodio.nome}</strong></p>`
+    eps.innerHTML = `<p>O episódio sugerido para assistir é da <strong>${nEpisodio.temporada}º Temporada, episódio ${nEpisodio.episodio}, ${nEpisodio.nome}</strong></p>`
 }
 
 
